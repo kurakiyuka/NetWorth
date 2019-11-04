93174222cedec984c841894387988ececaf512c0
YUUKI <myzlovebqb@sina.com>
2019-11-05 00:57:19 +0800

    k

--- net/urls.py
+++ net/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello', views.hello)
+    path('index', views.index),
+    path('insert', views.insert)
 ]
--- net/views.py
+++ net/views.py
@@ -4,8 +4,31 @@ from .models import NetModel
 
 # Create your views here.
 
-def hello(requset):
+def index(requset):
     all_netmodel = NetModel.objects.all()
+    if requset.POST:
+        post_name = requset.POST['name']
+        post_value = requset.POST['value']
+        for netmodel in all_netmodel:
+            if netmodel.name == post_name:
+                netmodel.total_price = int(post_value)
+                netmodel.save()
+                break
+
+    total_assets = 0
+    for netmodel in all_netmodel:
+        total_assets = total_assets + netmodel.total_price
+    
     return render(requset, 'index.html', {
-        'netmodel_list': all_netmodel
+        'netmodel_list': all_netmodel,
+        'total_assets': total_assets
+    })
+
+def insert(requset):
+    all_netmodel = NetModel.objects.all()
+    all_name = []
+    for netmodel in all_netmodel:
+        all_name.append(netmodel.name)
+    return render(requset, 'insert.html', {
+        'name_list': all_name
     })
