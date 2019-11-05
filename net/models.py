3c14b2dc02be393182b06612cdf09a83f1e8e109
YUUKI@Com <maoyunzhu@58.com>
2019-11-05 22:29:27 +0800

    a

--- net/models.py
+++ net/models.py
@@ -3,49 +3,53 @@ from django.db import models
 # Create your models here.
 
 class NetModel(models.Model):
+    ASSETS_CATEGORY_CHOICES = [('Cash', '现金'), ('Stocks', '股票'), ('Funds', '基金'), ('Near Cash', '类现金'), ('Insurance', '保险'), ('Credit', '信用卡'), ('Bonds', '债券'), ('Fixed', '定期理财'), ('Others', '其他理财')]
+    ASSETS_TYPE_CHOICES = [('Assets', '资产'), ('Debts', '负债')]
+    CURRENCY_TYPE_CHOICES = [('RMB', '人民币'), ('HKD', '港元'), ('USD', '美元')]
+
     # 资产账户名
-    name = models.CharField(max_length = 100)
+    assets_name = models.CharField(max_length = 100)
     # 资产账户分类
-    category = models.CharField(max_length = 100)
+    assets_category = models.CharField(max_length = 100, choices = ASSETS_CATEGORY_CHOICES, default = 'Cash')
     # 资产 / 负债
-    assets_type = models.CharField(max_length = 20)
+    assets_type = models.CharField(max_length = 20, choices = ASSETS_TYPE_CHOICES, default = 'Assets')
     # 币种
-    currency_type = models.CharField(max_length = 10, default = 'RMB')
+    currency_type = models.CharField(max_length = 10, choices = CURRENCY_TYPE_CHOICES, default = 'RMB')
     # 汇率
-    exchange_rate = models.IntegerField(default = 1)
+    exchange_rate = models.FloatField(default = 1.0)
     # 股票基金代号
-    code = models.CharField(max_length = 10, default = '000')
+    code = models.CharField(max_length = 10, default = '000000')
     # 股票数量、基金份额
-    amount = models.IntegerField(default = 0)
+    amount = models.FloatField(default = 0.0)
     # 股票基金单价
-    unit_price = models.IntegerField(default = 0)
+    unit_price = models.FloatField(default = 0.0)
     # 股票基金总价、资产总价
-    total_price = models.IntegerField(default = 0)
+    total_price = models.FloatField(default = 0.0)
     # 更新时间
     update_date = models.DateTimeField(auto_now = True)
 
     def __str__(self):
-        return self.name
+        return self.assets_name
 
 class TotalNetModel(models.Model):
     # 净资产
-    total = models.IntegerField()
+    total = models.FloatField()
     update_date = models.DateTimeField(auto_now = True)
 
     def __str__(self):
-        return self.update_date
+        return str(self.update_date)
 
 class ModifyModel(models.Model):
     # 被更改的资产账户
-    name = models.CharField(max_length = 100)
+    assets_name = models.CharField(max_length = 100)
     # 更改股票数量、基金份额
-    amount = models.IntegerField(default = 0)
+    amount = models.FloatField(default = 0.0)
     # 更改总价
-    total_price = models.IntegerField(default = 0)
+    total_price = models.FloatField(default = 0.0)
     # 增加 / 减少
     change_type = models.CharField(max_length = 20)
     # 增减的量
-    change_amount = models.IntegerField(default = 0)
+    change_amount = models.FloatField(default = 0.0)
 
     def __str__(self):
-        return self.name
+        return self.assets_name
--- net/views.py
+++ net/views.py
@@ -1,34 +1,60 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import NetModel
+import json
+from .models import NetModel, TotalNetModel
 
 # Create your views here.
 
+
 def index(requset):
     all_netmodel = NetModel.objects.all()
+    
     if requset.POST:
         post_name = requset.POST['name']
         post_value = requset.POST['value']
+        post_change_type = requset.POST['change_type']
         for netmodel in all_netmodel:
-            if netmodel.name == post_name:
-                netmodel.total_price = int(post_value)
+            if netmodel.assets_name == post_name:
+                if post_change_type == '+':
+                    netmodel.total_price += int(post_value)
+                else:
+                    netmodel.total_price -= int(post_value)
                 netmodel.save()
                 break
 
     total_assets = 0
+    new_totalnetmodel = TotalNetModel()
     for netmodel in all_netmodel:
-        total_assets = total_assets + netmodel.total_price
-    
+        if netmodel.assets_type == 'Assets':
+            if netmodel.currency_type != 'RMB':
+                netmodel.total_price = netmodel.total_price * netmodel.exchange_rate
+            total_assets = total_assets + netmodel.total_price
+        else:
+            total_assets = total_assets - netmodel.total_price
+    new_totalnetmodel.total = total_assets
+    if requset.POST:
+        new_totalnetmodel.save()
+
+    all_totalnetmodel = TotalNetModel.objects.all()
+    all_totalnetmodel_date = []
+    all_totalnetmodel_price = []
+    for totalnetmodel in all_totalnetmodel:
+        all_totalnetmodel_date.append(str(totalnetmodel.update_date))
+        all_totalnetmodel_date.append(totalnetmodel.total)
+
     return render(requset, 'index.html', {
         'netmodel_list': all_netmodel,
-        'total_assets': total_assets
+        'total_assets': total_assets,
+        'all_totalnetmodel_date': all_totalnetmodel_date,
+        'all_totalnetmodel_price': all_totalnetmodel_price
     })
+
 
 def insert(requset):
     all_netmodel = NetModel.objects.all()
     all_name = []
     for netmodel in all_netmodel:
-        all_name.append(netmodel.name)
+        all_name.append(netmodel.assets_name)
     return render(requset, 'insert.html', {
         'name_list': all_name
     })
